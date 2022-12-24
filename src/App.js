cf34193822dd90867d9d6c913e72d5d90e38e739
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-24 18:30:34 +0700

    fixed setup tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Stater page</h1>
+    <div>
+      <h1 className="text-center text-3xl font-bold underline">Stater page</h1>
     </div>
   );
 }
